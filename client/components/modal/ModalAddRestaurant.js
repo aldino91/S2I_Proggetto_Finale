2a6d75fd0ef6d95ee78e3e78e16fd5bac1e1d51c
aldino91 <aldobrunet@icloud.com>
2022-07-10 02:49:21 +0200

    aggiunta navbar al modal add restaurant

--- client/components/modal/ModalAddRestaurant.js
+++ client/components/modal/ModalAddRestaurant.js
@@ -1,11 +1,13 @@
 import React from "react";
 import FormAddRestaurant from "../form/FormAddRestaurant";
 import IconClose from "../icons/IconClose";
+import Navbar from "../navbar/Navbar";
 import BaseModal from "./BaseModal";
 
 export default function ModalAddRestaurant({ setOpenModal, setReload }) {
   return (
     <BaseModal>
+      <Navbar />
       <div
         onClick={() => setOpenModal(false)}
         className="absolute top-2 right-2"
